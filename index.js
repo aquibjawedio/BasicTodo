1f1d2bd38d80ec85d3395e0073d992d45245aa83
Aquib Jawed <aquibjawed.dev@gmail.com>
2025-02-06 12:36:00 +0530

    Uploading Files and Folders

---
+++ index.js
@@ -0,0 +1,32 @@
+//  Toggling Sidebar
+
+const toggleSidebar = document.querySelector('.toggleSidebar');
+const sidebar = document.querySelector('.sidebar');
+const appsetting = document.querySelector('.appsetting');
+const sidebarMenus = document.querySelector('.sidebar-menus');
+
+let sidebarHidden = true;
+
+toggleSidebar.addEventListener('click', () => {
+
+    if(sidebarHidden) {
+        toggleSidebar.style.transform = 'rotate(180deg)';
+        sidebar.style.width = '0';
+        appsetting.style.display = 'none';
+        sidebarMenus.style.display = 'none';
+        sidebarHidden = false;
+        
+    }else {
+        toggleSidebar.style.transform = 'rotate(0deg)';
+        appsetting.style.display = 'flex';
+        sidebarMenus.style.display = 'flex';
+        sidebar.style.width = '250px';
+        sidebarHidden = true;
+    }
+})
+
+
+
+
+
+
