08db81c3220e321097c8f9d1b87ea33ba467bd7e
Aquib Jawed <aquibjawed.dev@gmail.com>
2025-02-06 21:32:48 +0530

    Minor Changes in JS

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ let sidebarHidden = true;
 
 toggleSidebar.addEventListener('click', () => {
 
-    if(sidebarHidden) {
+    if (sidebarHidden) {
         toggleSidebar.style.transform = 'rotate(180deg)';
         sidebar.style.width = '0';
         appsetting.style.display = 'none';
         sidebarMenus.style.display = 'none';
         sidebarHidden = false;
-        
-    }else {
+
+    } else {
         toggleSidebar.style.transform = 'rotate(0deg)';
         appsetting.style.display = 'flex';
         sidebarMenus.style.display = 'flex';
@@ -28,9 +28,13 @@ toggleSidebar.addEventListener('click', () => {
 
 
 
+
+/**********************************************************************/
+
+
 // current date 
 
-const currentDate= document.querySelector('.currentDate');
+const currentDate = document.querySelector('.currentDate');
 
 const date = new Date();
 currentDate.textContent = date.toDateString();
@@ -40,3 +44,73 @@ currentDate.textContent = date.toDateString();
 
 
 
+/**********************************************************************/
+
+// Creating new task
+
+const addTask = document.querySelector('.addtask');
+const taskbar = document.querySelector('.taskbar');
+
+function createNewTask() {
+    const createTaskPopup = document.querySelector('.createTaskPopup');
+    createTaskPopup.style.visibility = 'hidden';
+
+    let taskTitle = document.querySelector('.task-title').value;
+    let taskContent = document.querySelector('.task-content').value;
+
+    if(!taskTitle.trim() || !taskContent.trim()) {
+        alert("Plese enter a valid input");
+        return 
+    }
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.setAttribute('tabindex', '0');
+
+    // Create the card-details div
+    const cardDetails = document.createElement('div');
+    cardDetails.classList.add('card-details');
+
+    // Create the title paragraph
+    const title = document.createElement('p');
+    title.classList.add('text-title');
+    title.textContent = taskTitle;
+    
+
+    // Create the task content
+    const content = document.createElement('p');
+    content.classList.add('text-body');
+    content.textContent = taskContent;
+
+    cardDetails.appendChild(title);
+    cardDetails.appendChild(content);
+    card.appendChild(cardDetails);
+
+    taskbar.appendChild(card);
+    return;
+
+}
+
+addTask.addEventListener('click', (event) => {
+
+    // event.preventDefault();
+
+    const createTaskPopup = document.querySelector('.createTaskPopup');
+
+    if (createTaskPopup.style.visibility == 'hidden') {
+        console.log("addTask clicked");
+        createTaskPopup.style.visibility = 'visible'
+
+        // Fetching user input
+        let submitTask = document.querySelector('.submit-task');
+
+        submitTask.addEventListener('click', createNewTask);
+
+    } else {
+        createTaskPopup.style.visibility = 'hidden';
+    }
+})
+
+
+
+/**********************************************************************/
